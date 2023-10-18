3b94d4432fd4c26c582ecdaa4dcafef84cc02df6
Andrew <andrewsolitei@gmail.com>
2023-10-19 00:07:24 +0300

    task6

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,9 @@
+-- Create the 'hbtn_0d_usa' database if it doesn't exist
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id),
+    UNIQUE KEY (id)
+);
