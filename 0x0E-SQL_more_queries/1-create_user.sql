72ca566026c82981a48c3bd9a89d5a3b65acb187
Andrew <andrewsolitei@gmail.com>
2023-10-18 23:41:09 +0300

    Update 1

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- Creates the MySQL server user user_0d_1 and grants all privileges to it.
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
