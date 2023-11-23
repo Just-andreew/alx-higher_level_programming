595db2977115412bd4ed2e3f35ebc876202efc3e
Just-andreew <andrewsolitei@gmail.com>
2023-11-24 00:32:55 +0300

    orm0

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+Script that lists all states from the database hbtn_0e_0_usa
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    # Database connection parameters
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    # Connect to MySQL server
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=db_name
+    )
+
+    # Create a cursor object to interact with the database
+    cursor = db.cursor()
+
+    # Execute the SQL query to retrieve states
+    cursor.execute("SELECT * FROM states ORDER BY id")
+
+    # Fetch all the rows
+    rows = cursor.fetchall()
+
+    # Display the results
+    for row in rows:
+        print(row)
+
+    # Close the cursor and database connection
+    cursor.close()
+    db.close()
+
