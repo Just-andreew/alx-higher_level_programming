4d4b1ba7860465de195de2847413722484e44fa3
Andrew <andrewsolitei@gmail.com>
2024-03-27 23:55:19 +0300

    0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-// reads and prints the content of a file
+// reads and prints the contents of a file
 const fs = require('fs');
 fs.readFile(process.argv[2], 'utf-8', (err, data) => {
   if (err) {
